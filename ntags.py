565fe7af41e77379b508667b0e2f972a62e68132
gs-yoon <57785895+gs-yoon@users.noreply.github.com>
2024-02-23 11:09:47 +0900

    add a modifying create time function

--- ntags.py
+++ ntags.py
@@ -1,7 +1,24 @@
 import os
+import pywintypes, win32file, win32con
 
-COMMAND = "attach"
+def changeFileCreationTime(fname, newtime):
+    wintime = pywintypes.Time(newtime)
+    winfile = win32file.CreateFile(
+        fname, win32con.GENERIC_WRITE,
+        win32con.FILE_SHARE_READ | win32con.FILE_SHARE_WRITE | win32con.FILE_SHARE_DELETE,
+        None, win32con.OPEN_EXISTING,
+        win32con.FILE_ATTRIBUTE_NORMAL, None)
+
+    win32file.SetFileTime(winfile, wintime, None, None)
+
+    winfile.close()
+
+
+# COMMAND = "attach"
 # COMMAND = "remove"
+COMMAND = "ctime"
+
+# eidtCtimeCmd = True
 
 cntFiles   = 0
 cntTouched = 0
@@ -14,23 +31,28 @@ for ntag in ntags :
     files = os.listdir(f'./{ntag}')
     for file in files:
         name, ext = os.path.splitext(file)
+        dirpath = f'./{ntag}/'
         cntFiles +=1
 
         if COMMAND == 'attach':
             if "#ntag" in name:
                 continue
-            dirpath = f'./{ntag}/'
             newfname = f'{name}#ntag_{ntag}{ext}'
             os.rename(dirpath + file, dirpath + newfname )
             cntTouched+=1
-        else: #remove 
+        elif COMMAND =='remove': #remove 
             if "#ntag" not in name:
                 continue
-            dirpath = f'./{ntag}/'
             idx = name.find('#ntag')
             newfname = f'{name[:idx]}{ext}'
             os.rename(dirpath + file, dirpath + newfname )
             cntTouched+=1
         
+        elif COMMAND =='ctime': #remove 
+            changeFileCreationTime(dirpath + file,  os.path.getmtime(dirpath + file))
+            cntTouched+=1
+
+        else:
+            break
 
 print(f"!! {cntTouched}/{cntFiles} is completed !!")
